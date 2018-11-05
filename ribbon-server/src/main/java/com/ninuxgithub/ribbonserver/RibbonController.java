3b3162460e11d80ff184f83ed2721f580e0c5414
ninuxGithub <1732768613@qq.com>
2018-11-05 15:04:15 +0800

    +zuul +feign

--- ribbon-server/src/main/java/com/ninuxgithub/ribbonserver/RibbonController.java
+++ ribbon-server/src/main/java/com/ninuxgithub/ribbonserver/RibbonController.java
@@ -1,22 +1,22 @@
-//package com.ninuxgithub.ribbonserver;
-//
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//import java.util.Map;
-//
-//@RestController
-//public class RibbonController {
-//
-//    @Autowired
-//    RibbonService ribbonService;
-//
-//    @GetMapping("/testSave")
-//    public Map<String, Boolean> savePerson() {
-//        return ribbonService.savePerson();
-//    }
-//
-//}
+package com.ninuxgithub.ribbonserver;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class RibbonController {
+
+    @Autowired
+    RibbonService ribbonService;
+
+    @GetMapping("/testSave")
+    public Map<String, Boolean> savePerson() {
+        return ribbonService.savePerson();
+    }
+
+}
